7e517be060017ff89ae2bb9c8d579d5d845b69ba
Mickaël <mickael.debraekeleer@student.unamur.be>
2024-11-18 14:56:29 +0100

    Update console_gr_20.py

--- console_gr_20.py
+++ console_gr_20.py
@@ -18,7 +18,26 @@ def get_message() -> str:
         message=radio.receive()
         
     return message
-
+def generate_board():
+    #wall coordonate virtual
+    wall = [[1,1],[2,1],[3,1],[7,1],[2,3],[2,4],[5,3],[6,3],[7,3],[8,3],[1,7],[1,8],[5,6],[6,6],[7,6],[6,7],[6,8],[6,9],[9,8],[9,9]]
+    player = [4, 4]
+    cat = [5, 5]
+          
+    return wall, player, cat
+def get_local_view(wall, player, cat):
+    map=str()
+    for x in range(player[0]-2, player[0]+3):
+        for y in range(player[1]-2, player[1]+3):
+            if [x, y] in wall:
+                map+="w"
+            elif [x, y] in player:
+                map+="p"
+            elif [x, y] in cat:
+                map+="c"
+            else:
+                map+="v"
+    return map
 #settings
 group_id = 20
 size = 10
@@ -28,9 +47,10 @@ radio.on()
 radio.config(group=group_id)
 
 #create board an place cat + player
+wall, player, cat = generate_board()
 
 #send local view of the board to gamepad
-local_view="test"
+local_view=get_local_view(wall, player, cat)
 radio.send(local_view)
 
 #loop until game is over
@@ -43,7 +63,7 @@ while not game_is_over:
     #execute order (move player)
     
     #send local view of the board to gamepad
-    local_view="test"
+    local_view=get_local_view(wall, player, cat)
     radio.send(local_view)
     
     #check if game is over
