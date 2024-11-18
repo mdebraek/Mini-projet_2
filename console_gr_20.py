67053b1141f5258c62726104104e64d1e5c667c0
Mickaël <mickael.debraekeleer@student.unamur.be>
2024-11-18 15:00:38 +0100

    Update console_gr_20.py

--- console_gr_20.py
+++ console_gr_20.py
@@ -19,6 +19,13 @@ def get_message() -> str:
         
     return message
 def generate_board():
+    """ generate the map, the wall, the position of the pkayer and the cat
+    Returns
+    -------
+    wall: the list of the position of all walls(list)
+    player: the list of the position of the player(list)
+    cat: the list of the position of the cat(list)
+    """
     #wall coordonate virtual
     wall = [[1,1],[2,1],[3,1],[7,1],[2,3],[2,4],[5,3],[6,3],[7,3],[8,3],[1,7],[1,8],[5,6],[6,6],[7,6],[6,7],[6,8],[6,9],[9,8],[9,9]]
     player = [4, 4]
