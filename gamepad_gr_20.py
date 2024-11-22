706f7bc5f868cc3baffa05e7d6424bd0c557e9c2
Mickaël <mickael.debraekeleer@student.unamur.be>
2024-11-22 19:59:29 +0100

    Update gamepad_gr_20.py

--- gamepad_gr_20.py
+++ gamepad_gr_20.py
@@ -59,14 +59,14 @@ while True:
     
     x_strength = microbit.accelerometer.get_x()
     y_strength = microbit.accelerometer.get_y()
-    deadzone=250
+    deadzone=100
     direction=str()
     if abs(x_strength)>deadzone or abs(y_strength)>deadzone:
         if abs(x_strength)>abs(y_strength):
             if x_strength>0:
-                direction="left"
-            else:
                 direction="right"
+            else:
+                direction="left"
         else:
             if y_strength>0:
                 direction="up"
